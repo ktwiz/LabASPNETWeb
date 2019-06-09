9c23c84c0faf72f3e4aa18f5dd26d4ddb3b86002
TuanDH <tuandhhso@acb.com.vn>
2019-06-09 17:59:05 +0700

    #6: Add layout & add field short desc & complete home page

--- LabASPNETWeb/LabASPNETWeb/DataRepository/Linq/DB.designer.cs
+++ LabASPNETWeb/LabASPNETWeb/DataRepository/Linq/DB.designer.cs
@@ -30,12 +30,12 @@ namespace LabASPNETWeb.DataRepository.Linq
 		
     #region Extensibility Method Definitions
     partial void OnCreated();
-    partial void InsertwpNew(wpNew instance);
-    partial void UpdatewpNew(wpNew instance);
-    partial void DeletewpNew(wpNew instance);
     partial void InsertwpUser(wpUser instance);
     partial void UpdatewpUser(wpUser instance);
     partial void DeletewpUser(wpUser instance);
+    partial void InsertwpNew(wpNew instance);
+    partial void UpdatewpNew(wpNew instance);
+    partial void DeletewpNew(wpNew instance);
     #endregion
 		
 		public DBDataContext() : 
@@ -68,14 +68,6 @@ namespace LabASPNETWeb.DataRepository.Linq
 			OnCreated();
 		}
 		
-		public System.Data.Linq.Table<wpNew> wpNews
-		{
-			get
-			{
-				return this.GetTable<wpNew>();
-			}
-		}
-		
 		public System.Data.Linq.Table<wpUser> wpUsers
 		{
 			get
@@ -83,299 +75,12 @@ namespace LabASPNETWeb.DataRepository.Linq
 				return this.GetTable<wpUser>();
 			}
 		}
-	}
-	
-	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.wpNews")]
-	public partial class wpNew : INotifyPropertyChanging, INotifyPropertyChanged
-	{
 		
-		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
-		
-		private int _id;
-		
-		private string _title;
-		
-		private string _content;
-		
-		private string _tags;
-		
-		private string _usr_create;
-		
-		private System.Nullable<System.DateTime> _createdt;
-		
-		private System.Nullable<System.DateTime> _lastupdt;
-		
-		private System.Nullable<bool> _activeyn;
-		
-		private System.Nullable<bool> _homepageyn;
-		
-		private EntityRef<wpUser> _wpUser;
-		
-    #region Extensibility Method Definitions
-    partial void OnLoaded();
-    partial void OnValidate(System.Data.Linq.ChangeAction action);
-    partial void OnCreated();
-    partial void OnidChanging(int value);
-    partial void OnidChanged();
-    partial void OntitleChanging(string value);
-    partial void OntitleChanged();
-    partial void OncontentChanging(string value);
-    partial void OncontentChanged();
-    partial void OntagsChanging(string value);
-    partial void OntagsChanged();
-    partial void Onusr_createChanging(string value);
-    partial void Onusr_createChanged();
-    partial void OncreatedtChanging(System.Nullable<System.DateTime> value);
-    partial void OncreatedtChanged();
-    partial void OnlastupdtChanging(System.Nullable<System.DateTime> value);
-    partial void OnlastupdtChanged();
-    partial void OnactiveynChanging(System.Nullable<bool> value);
-    partial void OnactiveynChanged();
-    partial void OnhomepageynChanging(System.Nullable<bool> value);
-    partial void OnhomepageynChanged();
-    #endregion
-		
-		public wpNew()
-		{
-			this._wpUser = default(EntityRef<wpUser>);
-			OnCreated();
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
-		public int id
+		public System.Data.Linq.Table<wpNew> wpNews
 		{
 			get
 			{
-				return this._id;
-			}
-			set
-			{
-				if ((this._id != value))
-				{
-					this.OnidChanging(value);
-					this.SendPropertyChanging();
-					this._id = value;
-					this.SendPropertyChanged("id");
-					this.OnidChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_title", DbType="NVarChar(200)")]
-		public string title
-		{
-			get
-			{
-				return this._title;
-			}
-			set
-			{
-				if ((this._title != value))
-				{
-					this.OntitleChanging(value);
-					this.SendPropertyChanging();
-					this._title = value;
-					this.SendPropertyChanged("title");
-					this.OntitleChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_content", DbType="NText", UpdateCheck=UpdateCheck.Never)]
-		public string content
-		{
-			get
-			{
-				return this._content;
-			}
-			set
-			{
-				if ((this._content != value))
-				{
-					this.OncontentChanging(value);
-					this.SendPropertyChanging();
-					this._content = value;
-					this.SendPropertyChanged("content");
-					this.OncontentChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_tags", DbType="VarChar(255)")]
-		public string tags
-		{
-			get
-			{
-				return this._tags;
-			}
-			set
-			{
-				if ((this._tags != value))
-				{
-					this.OntagsChanging(value);
-					this.SendPropertyChanging();
-					this._tags = value;
-					this.SendPropertyChanged("tags");
-					this.OntagsChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_usr_create", DbType="VarChar(100)")]
-		public string usr_create
-		{
-			get
-			{
-				return this._usr_create;
-			}
-			set
-			{
-				if ((this._usr_create != value))
-				{
-					if (this._wpUser.HasLoadedOrAssignedValue)
-					{
-						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
-					}
-					this.Onusr_createChanging(value);
-					this.SendPropertyChanging();
-					this._usr_create = value;
-					this.SendPropertyChanged("usr_create");
-					this.Onusr_createChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_createdt", DbType="DateTime")]
-		public System.Nullable<System.DateTime> createdt
-		{
-			get
-			{
-				return this._createdt;
-			}
-			set
-			{
-				if ((this._createdt != value))
-				{
-					this.OncreatedtChanging(value);
-					this.SendPropertyChanging();
-					this._createdt = value;
-					this.SendPropertyChanged("createdt");
-					this.OncreatedtChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_lastupdt", DbType="DateTime")]
-		public System.Nullable<System.DateTime> lastupdt
-		{
-			get
-			{
-				return this._lastupdt;
-			}
-			set
-			{
-				if ((this._lastupdt != value))
-				{
-					this.OnlastupdtChanging(value);
-					this.SendPropertyChanging();
-					this._lastupdt = value;
-					this.SendPropertyChanged("lastupdt");
-					this.OnlastupdtChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_activeyn", DbType="Bit")]
-		public System.Nullable<bool> activeyn
-		{
-			get
-			{
-				return this._activeyn;
-			}
-			set
-			{
-				if ((this._activeyn != value))
-				{
-					this.OnactiveynChanging(value);
-					this.SendPropertyChanging();
-					this._activeyn = value;
-					this.SendPropertyChanged("activeyn");
-					this.OnactiveynChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_homepageyn", DbType="Bit")]
-		public System.Nullable<bool> homepageyn
-		{
-			get
-			{
-				return this._homepageyn;
-			}
-			set
-			{
-				if ((this._homepageyn != value))
-				{
-					this.OnhomepageynChanging(value);
-					this.SendPropertyChanging();
-					this._homepageyn = value;
-					this.SendPropertyChanged("homepageyn");
-					this.OnhomepageynChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="wpUser_wpNew", Storage="_wpUser", ThisKey="usr_create", OtherKey="username", IsForeignKey=true)]
-		public wpUser wpUser
-		{
-			get
-			{
-				return this._wpUser.Entity;
-			}
-			set
-			{
-				wpUser previousValue = this._wpUser.Entity;
-				if (((previousValue != value) 
-							|| (this._wpUser.HasLoadedOrAssignedValue == false)))
-				{
-					this.SendPropertyChanging();
-					if ((previousValue != null))
-					{
-						this._wpUser.Entity = null;
-						previousValue.wpNews.Remove(this);
-					}
-					this._wpUser.Entity = value;
-					if ((value != null))
-					{
-						value.wpNews.Add(this);
-						this._usr_create = value.username;
-					}
-					else
-					{
-						this._usr_create = default(string);
-					}
-					this.SendPropertyChanged("wpUser");
-				}
-			}
-		}
-		
-		public event PropertyChangingEventHandler PropertyChanging;
-		
-		public event PropertyChangedEventHandler PropertyChanged;
-		
-		protected virtual void SendPropertyChanging()
-		{
-			if ((this.PropertyChanging != null))
-			{
-				this.PropertyChanging(this, emptyChangingEventArgs);
-			}
-		}
-		
-		protected virtual void SendPropertyChanged(String propertyName)
-		{
-			if ((this.PropertyChanged != null))
-			{
-				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+				return this.GetTable<wpNew>();
 			}
 		}
 	}
@@ -635,6 +340,325 @@ namespace LabASPNETWeb.DataRepository.Linq
 		{
 			this.SendPropertyChanging();
 			entity.wpUser = null;
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.wpNews")]
+	public partial class wpNew : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _id;
+		
+		private string _title;
+		
+		private string _content;
+		
+		private string _tags;
+		
+		private string _usr_create;
+		
+		private System.Nullable<System.DateTime> _createdt;
+		
+		private System.Nullable<System.DateTime> _lastupdt;
+		
+		private System.Nullable<bool> _activeyn;
+		
+		private System.Nullable<bool> _homepageyn;
+		
+		private string _shortdesc;
+		
+		private EntityRef<wpUser> _wpUser;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnidChanging(int value);
+    partial void OnidChanged();
+    partial void OntitleChanging(string value);
+    partial void OntitleChanged();
+    partial void OncontentChanging(string value);
+    partial void OncontentChanged();
+    partial void OntagsChanging(string value);
+    partial void OntagsChanged();
+    partial void Onusr_createChanging(string value);
+    partial void Onusr_createChanged();
+    partial void OncreatedtChanging(System.Nullable<System.DateTime> value);
+    partial void OncreatedtChanged();
+    partial void OnlastupdtChanging(System.Nullable<System.DateTime> value);
+    partial void OnlastupdtChanged();
+    partial void OnactiveynChanging(System.Nullable<bool> value);
+    partial void OnactiveynChanged();
+    partial void OnhomepageynChanging(System.Nullable<bool> value);
+    partial void OnhomepageynChanged();
+    partial void OnshortdescChanging(string value);
+    partial void OnshortdescChanged();
+    #endregion
+		
+		public wpNew()
+		{
+			this._wpUser = default(EntityRef<wpUser>);
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int id
+		{
+			get
+			{
+				return this._id;
+			}
+			set
+			{
+				if ((this._id != value))
+				{
+					this.OnidChanging(value);
+					this.SendPropertyChanging();
+					this._id = value;
+					this.SendPropertyChanged("id");
+					this.OnidChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_title", DbType="NVarChar(200)")]
+		public string title
+		{
+			get
+			{
+				return this._title;
+			}
+			set
+			{
+				if ((this._title != value))
+				{
+					this.OntitleChanging(value);
+					this.SendPropertyChanging();
+					this._title = value;
+					this.SendPropertyChanged("title");
+					this.OntitleChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_content", DbType="NText", UpdateCheck=UpdateCheck.Never)]
+		public string content
+		{
+			get
+			{
+				return this._content;
+			}
+			set
+			{
+				if ((this._content != value))
+				{
+					this.OncontentChanging(value);
+					this.SendPropertyChanging();
+					this._content = value;
+					this.SendPropertyChanged("content");
+					this.OncontentChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_tags", DbType="VarChar(255)")]
+		public string tags
+		{
+			get
+			{
+				return this._tags;
+			}
+			set
+			{
+				if ((this._tags != value))
+				{
+					this.OntagsChanging(value);
+					this.SendPropertyChanging();
+					this._tags = value;
+					this.SendPropertyChanged("tags");
+					this.OntagsChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_usr_create", DbType="VarChar(100)")]
+		public string usr_create
+		{
+			get
+			{
+				return this._usr_create;
+			}
+			set
+			{
+				if ((this._usr_create != value))
+				{
+					if (this._wpUser.HasLoadedOrAssignedValue)
+					{
+						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
+					}
+					this.Onusr_createChanging(value);
+					this.SendPropertyChanging();
+					this._usr_create = value;
+					this.SendPropertyChanged("usr_create");
+					this.Onusr_createChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_createdt", DbType="DateTime")]
+		public System.Nullable<System.DateTime> createdt
+		{
+			get
+			{
+				return this._createdt;
+			}
+			set
+			{
+				if ((this._createdt != value))
+				{
+					this.OncreatedtChanging(value);
+					this.SendPropertyChanging();
+					this._createdt = value;
+					this.SendPropertyChanged("createdt");
+					this.OncreatedtChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_lastupdt", DbType="DateTime")]
+		public System.Nullable<System.DateTime> lastupdt
+		{
+			get
+			{
+				return this._lastupdt;
+			}
+			set
+			{
+				if ((this._lastupdt != value))
+				{
+					this.OnlastupdtChanging(value);
+					this.SendPropertyChanging();
+					this._lastupdt = value;
+					this.SendPropertyChanged("lastupdt");
+					this.OnlastupdtChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_activeyn", DbType="Bit")]
+		public System.Nullable<bool> activeyn
+		{
+			get
+			{
+				return this._activeyn;
+			}
+			set
+			{
+				if ((this._activeyn != value))
+				{
+					this.OnactiveynChanging(value);
+					this.SendPropertyChanging();
+					this._activeyn = value;
+					this.SendPropertyChanged("activeyn");
+					this.OnactiveynChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_homepageyn", DbType="Bit")]
+		public System.Nullable<bool> homepageyn
+		{
+			get
+			{
+				return this._homepageyn;
+			}
+			set
+			{
+				if ((this._homepageyn != value))
+				{
+					this.OnhomepageynChanging(value);
+					this.SendPropertyChanging();
+					this._homepageyn = value;
+					this.SendPropertyChanged("homepageyn");
+					this.OnhomepageynChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_shortdesc", DbType="NVarChar(200)")]
+		public string shortdesc
+		{
+			get
+			{
+				return this._shortdesc;
+			}
+			set
+			{
+				if ((this._shortdesc != value))
+				{
+					this.OnshortdescChanging(value);
+					this.SendPropertyChanging();
+					this._shortdesc = value;
+					this.SendPropertyChanged("shortdesc");
+					this.OnshortdescChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="wpUser_wpNew", Storage="_wpUser", ThisKey="usr_create", OtherKey="username", IsForeignKey=true)]
+		public wpUser wpUser
+		{
+			get
+			{
+				return this._wpUser.Entity;
+			}
+			set
+			{
+				wpUser previousValue = this._wpUser.Entity;
+				if (((previousValue != value) 
+							|| (this._wpUser.HasLoadedOrAssignedValue == false)))
+				{
+					this.SendPropertyChanging();
+					if ((previousValue != null))
+					{
+						this._wpUser.Entity = null;
+						previousValue.wpNews.Remove(this);
+					}
+					this._wpUser.Entity = value;
+					if ((value != null))
+					{
+						value.wpNews.Add(this);
+						this._usr_create = value.username;
+					}
+					else
+					{
+						this._usr_create = default(string);
+					}
+					this.SendPropertyChanged("wpUser");
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
 		}
 	}
 }
